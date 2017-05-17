435c9639391647e7db4dfe89900f4f2571da0ae9
Alex Simonian <alex.simonian@gmail.com>
2017-05-17 13:30:30 -0500

    Add entry point

--- Counter.elm
+++ Counter.elm
@@ -1,2 +1,8 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+
+
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
