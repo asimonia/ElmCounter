913c9f6c63cdcc6335fdbf73b5c357adcbc3d5a0
Alex Simonian <alex.simonian@gmail.com>
2017-05-17 13:44:40 -0500

    Add model

--- Counter.elm
+++ Counter.elm
@@ -28,3 +28,13 @@ model =
 type Msg
     = Increment
     | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
